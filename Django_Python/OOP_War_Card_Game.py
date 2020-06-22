9133dc97c3ca881edb132aa1aaf0a4c158ab6b78
Anh Le <anhle_2021@depauw.edu>
2020-06-22 15:41:29 -0400

    Deck Class Created with shuffle and split in 2 hands functions completed

--- Django_Python/OOP_War_Card_Game.py
+++ Django_Python/OOP_War_Card_Game.py
@@ -1,25 +1,3 @@
-# For this project you will be using OOP to create a card game. This card game will
-# be the card game "War" for two players, you an the computer. If you don't know
-# how to play "War" here are the basic rules:
-#
-# The deck is divided evenly, with each player receiving 26 cards, dealt one at a time,
-# face down. Anyone may deal first. Each player places his stack of cards face down,
-# in front of him.
-#
-# The Play:
-#
-# Each player turns up a card at the same time and the player with the higher card
-# takes both cards and puts them, face down, on the bottom of his stack.
-#
-# If the cards are the same rank, it is War. Each player turns up three cards face
-# down and one card face up. The player with the higher cards takes both piles
-# (six cards). If the turned-up cards are again the same rank, each player places
-# another card face down and turns another card face up. The player with the
-# higher card takes all 10 cards, and so on.
-#
-# There are some more variations on this but we will keep it simple for now.
-# Ignore "double" wars
-#
 # https://en.wikipedia.org/wiki/War_(card_game)
 
 from random import shuffle
@@ -35,7 +13,16 @@ class Deck:
     the players. It will use SUITE and RANKS to create the deck. It should also
     have a method for splitting/cutting the deck in half and Shuffling the deck.
     """
-    pass
+    def __init__(self):
+        print("Creating A New Deck of Cards")
+        self.cards = [(s,r) for s in SUITE for r in RANKS ]
+    def __str__(self):
+        print(self.cards)
+        return  ""
+    def shuffle(self):
+        shuffle(self.cards)
+    def split(self):
+        return (self.cards[:26], self.cards[26:])
 
 class Hand:
     '''
@@ -51,10 +38,9 @@ class Player:
     """
     pass
 
-
-######################
-#### GAME PLAY #######
-######################
 print("Welcome to War, let's begin...")
-
-# Use the 3 classes along with some logic to play a game of war!
+deck = Deck()
+deck.shuffle()
+half1, half2 = deck.split()
+print(half1)
+print(half2)
