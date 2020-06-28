a39e5bb6be0e7caf39d3ae815d10ee45eb29aeca
Anh Le <anhle_2021@depauw.edu>
2020-06-28 18:46:31 -0400

    OOP Project War (Card Game) in Python Finished

--- Django_Python/OOP_War_Card_Game.py
+++ Django_Python/OOP_War_Card_Game.py
@@ -73,10 +73,11 @@ print()
 computer = Player("Computer", Hand(half1))          #Computer takes 1st half of the Deck
 user = Player(userName, Hand(half2))                #User takes 2nd half of the Deck
 i = 0
-while user.still_has_cards() and computer.still_has_cards() and i < 3:
+while user.still_has_cards() and computer.still_has_cards():
+    print('\n')
     print("Here are the current standings: ")
-    print(user.name+"'s count: "+str(len(user.Hand.cards)))
-    print(computer.name+"'s count: "+str(len(computer.Hand.cards)))
+    print(user.name+"'s count: " + str(len(user.Hand.cards)))
+    print(computer.name+"'s count: " + str(len(computer.Hand.cards)))
     print()
 
     print(computer.name, ":", half1, '\n')
@@ -92,14 +93,42 @@ while user.still_has_cards() and computer.still_has_cards() and i < 3:
     playedCards.append(compCard)
 
     if(userCard[0] == compCard[0]):
-        print("There is a match. Time for War!")
+        print("------------------------------There is a match. Time for War!------------------------------")
         print("Each player removes 3 cards 'face down' and then one card face up")
+
+        if(len(user.Hand.cards) >= 3 and len(computer.Hand.cards) >= 3):
+            for x in range(3):
+                playedCards.append(user.Hand.cards.pop())
+                playedCards.append(computer.Hand.cards.pop())
+        else:
+            playedCards.extend(user.Hand.cards)
+            playedCards.extend(computer.Hand.cards)
+
+        userCard = user.play_card()
+        compCard = computer.play_card()
+        playedCards.append(userCard)
+        playedCards.append(compCard)
+        if(RANKS.index(userCard[0]) > RANKS.index(compCard[0])):
+            print(user.name+" has the higher card, adding to hand.")
+            for card in playedCards:
+                user.Hand.add(card)
+        else:
+            print(computer.name+" has the higher card, adding to hand.")
+            for card in playedCards:
+                computer.Hand.add(card)
 
     else:
         if(RANKS.index(userCard[0]) > RANKS.index(compCard[0])):
             print(user.name+" has the higher card, adding to hand.")
-            user.Hand.add(card in playedCards)
+            for card in playedCards:
+                user.Hand.add(card)
         else:
             print(computer.name+" has the higher card, adding to hand.")
-            computer.Hand.add(card in playedCards)
-    i+=1
+            for card in playedCards:
+                computer.Hand.add(card)
+
+print('\n')
+if(len(user.Hand.cards) > len(computer.Hand.cards)):
+    print("GAME OVER!", userName.upper(), "WINS !")
+else:
+    print("GAME OVER. COMPUTER WINS !")
