144ae8edf5e850db600e5be5bebe91e70c94d3a2
Anh Le <anhle_2021@depauw.edu>
2020-07-05 18:26:35 -0400

    Django Admin Interface

--- Django/first_project/first_app/admin.py
+++ Django/first_project/first_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from first_app.models import Topic, Webpage, AccessRecord
 # Register your models here.
+admin.sites.register(Topic)
+admin.sites.register(Webpage)
+admin.sites.register(AccessRecord)
