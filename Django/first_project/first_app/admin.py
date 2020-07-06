13db1c4450364a575a3355702e73b4e2cf57781f
Anh Le <anhle_2021@depauw.edu>
2020-07-06 00:15:05 -0400

    Django Administration Setup Completed

--- Django/first_project/first_app/admin.py
+++ Django/first_project/first_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from first_app.models import Topic, Webpage, AccessRecord
+
 # Register your models here.
-admin.sites.register(Topic)
-admin.sites.register(Webpage)
-admin.sites.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
+admin.site.register(AccessRecord)
