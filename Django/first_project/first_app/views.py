e9bee94f81dca13634890d9194855f7457283bc7
Anh Le <anhle_2021@depauw.edu>
2020-07-06 01:19:19 -0400

    Models-Templates-Views Paradigm

--- Django/first_project/first_app/views.py
+++ Django/first_project/first_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 # Create your views here.
 
 def index(request):
-    # return HttpResponse("<em>My First Project</em>")
-    my_dict = {'insert_me':"My name is Anh Le. Nice to Meet You!"}
-    return render(request, 'first_app/index.html', context = my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records' : webpages_list}
+    return render(request, 'first_app/index.html', context = date_dict)
+    # my_dict = {'insert_me':"My name is Anh Le. Nice to Meet You!"}
+    # return render(request, 'first_app/index.html', context = my_dict)
 
 def index2(request):
     return HttpResponse("<em>My First Project</em>")
