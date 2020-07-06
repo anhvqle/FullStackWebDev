ddddac16e487622752e81eb81155d52210719982
Anh Le <anhle_2021@depauw.edu>
2020-07-06 00:50:07 -0400

    Populating Scripts using Faker Library

---
+++ Django/first_project/populate_first_app.py
@@ -0,0 +1,38 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_project.settings')
+
+import django
+django.setup()
+
+import random
+from first_app.models import Topic, Webpage, AccessRecord
+from faker import Faker
+
+fakeGen = Faker()
+topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
+
+def add_topic():
+    t = Topic.objects.get_or_create(topic_name = random.choice(topics))[0]
+    t.save()
+    return t
+
+def populate(N = 5):
+    for entry in range (N):
+        # get the topic for the entry
+        top = add_topic()
+
+        # Create the fake data for that entry
+        fake_url = fakeGen.url()
+        fake_date = fakeGen.date()
+        fake_name = fakeGen.company()
+
+        # Create the new webpage entry
+        webpg = Webpage.objects.get_or_create(topic = top, url = fake_url, name = fake_name)[0]
+
+        # Create a fake access record for that webpage
+        acc_rec = AccessRecord.objects.get_or_create(name = webpg, date = fake_date)[0]
+
+if __name__ == '__main__':
+    print("populating script...")
+    populate(20)
+    print("populating completed!")
