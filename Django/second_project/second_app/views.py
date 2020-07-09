e04ddcfc8a6b5223e59dd45b93f4e59733ed26b5
Anh Le <anhle_2021@depauw.edu>
2020-07-09 14:21:03 -0400

    Django URL Mapping

--- Django/second_project/second_app/views.py
+++ Django/second_project/second_app/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from second_app.models import User
+from second_app.forms import NewUserForm
 
 def home(request):
     return render(request, 'second_app/home.html')
 
 def userInfo(request):
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users_info' : user_list}
-    return render(request, 'second_app/user.html', context = user_dict)
+    # user_list = User.objects.order_by('first_name')
+    # user_dict = {'users_info' : user_list}
+    # return render(request, 'second_app/user.html', context = user_dict)
+    form = NewUserForm()
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save(commit = True)
+            return home(request)
+        else:
+            print("ERROR FORM INVALID")
+
+    return render(request, 'second_app/user.html', {'form' : form})
 
 # Create your views here.
