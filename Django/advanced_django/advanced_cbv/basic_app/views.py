16f0b906f3c41287989235cf83293e0ff7a6127c
Anh Le <anhle_2021@depauw.edu>
2020-07-26 18:10:43 -0400

    Template Views with CBV

--- Django/advanced_django/advanced_cbv/basic_app/views.py
+++ Django/advanced_django/advanced_cbv/basic_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, TemplateView
 from django.http import HttpResponse
 # def index(request):
 #     return render(request, 'index.html')
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['inject_me'] = 'BASIC INJECTION'
+        return context
 
 class CBView(View):
     def get(self, request):
